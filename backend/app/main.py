8f1b036ca92475316e9e968b538efc37f38a2c3b
shubhg04 <shubhg2604@gmail.com>
2026-05-03 18:28:39 +0530

    Backend architecture, PostgreSQL setup, and database connection

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from sqlalchemy import text
+
+from backend.app.database import SessionLocal
 
 app = FastAPI()
 
 
 @app.get("/")
 def home():
-    return {"message": "Job Application Tracker API is running"}
+    return {"message": "Job Application Tracker API is running!"}
+
+
+@app.get("/db-test")
+def db_test():
+    db = SessionLocal()
+
+    try:
+        db.execute(text("SELECT 1"))
+        return {"message": "Database connection successful"}
+    finally:
+        db.close()
